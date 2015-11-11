33ebc39c4b2d175a3cd5119a9e4d02f8f11ee156
Seong-ho Cho <shcho@gnome.org>
2015-11-11 20:32:57 +0900

    improve GentooPackageFinder

--- ManalithBot/src/main/java/org/manalith/ircbot/plugin/linuxpkgfinder/GentooPackageFinder.java
+++ ManalithBot/src/main/java/org/manalith/ircbot/plugin/linuxpkgfinder/GentooPackageFinder.java
@@ -18,6 +18,11 @@
  */
 package org.manalith.ircbot.plugin.linuxpkgfinder;
 
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.manalith.ircbot.annotation.Option;
 import org.manalith.ircbot.common.stereotype.BotCommand;
 import org.springframework.stereotype.Component;
@@ -26,7 +31,6 @@ import org.springframework.stereotype.Component;
 public class GentooPackageFinder extends PackageFinder {
 
 	protected String keyword;
-	protected GentooSearchEngineProvider provider;
 
 	@Override
 	public String getName() {
@@ -38,18 +42,115 @@ public class GentooPackageFinder extends PackageFinder {
 		return "지정한 이름을 가진 젠투의 패키지를 검색합니다.";
 	}
 
-	public void setProvider(GentooSearchEngineProvider provider) {
-		this.provider = provider;
-	}
-
 	public String getKeyword() {
 		return keyword;
 	}
 
 	@Override
 	@BotCommand("gen")
-	public String find(
-			@Option(name = "키워드", help = "검색할 단어") String keyword) {
-		return provider.find(keyword);
+	public String find(@Option(name = "키워드", help = "검색할 단어") String keyword) {
+
+		String result = "";
+		String url = "https://packages.gentoo.org/packages/search?q=" + keyword;
+
+		try {
+			Element elem = Jsoup.connect(url).get().body();
+
+			if (isResultListPage(elem)) {
+				Elements elemlist = elem
+						.select("div.list-group>a.list-group-item");
+				for (Element el : elemlist) {
+
+					if (el.select("h3.kk-search-result-header").get(0).text()
+							.split("\\/")[1].equals(keyword)) {
+						result = getPackageResult(el.attr("href"));
+						break;
+					}
+				}
+
+				if (result.isEmpty())
+					result = createNoResultString();
+			} else if (isResultPage(elem)) {
+				String pkg = getCanonicalPackageName(elem);
+				String ver = getLatestVersion(elem);
+				String desc = getPackageDescription(elem);
+
+				result = createResultString(pkg, ver, desc);
+			} else
+				result = createNoResultString();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			result = createNoResultString();
+		}
+
+		return result;
+	}
+
+	public boolean isResultListPage(Element elem) {
+		return (elem.select("h3.kk-search-result-header").size() > 0);
+	}
+
+	public boolean isResultPage(Element elem) {
+		return (elem.select("h1.stick-top.kk-package-title").size() > 0);
+	}
+
+	public String getPackageResult(String suburl) {
+
+		String result = "";
+		try {
+			Element elem = Jsoup
+					.connect("https://packages.gentoo.org" + suburl).get()
+					.body();
+
+			String pkg = getCanonicalPackageName(elem);
+			String ver = getLatestVersion(elem);
+			String desc = getPackageDescription(elem);
+
+			result = createResultString(pkg, ver, desc);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			result = createNoResultString();
+		}
+
+		return result;
+	}
+
+	public String getCanonicalPackageName(Element e) {
+		Elements pNameArr = e.select("ol.breadcrumb>li");
+		String category = pNameArr.get(2).select("a").get(0).text();
+		String pkgname = pNameArr.get(3).text();
+
+		return category + "/" + pkgname;
+	}
+
+	public String getPackageDescription(Element e) {
+		return e.select("p.lead.kk-package-maindesc").get(0).text();
+	}
+
+	public String getLatestVersion(Element e) {
+		String result = "";
+		Elements pTableArr = e
+				.select("table.table.table-bordered.kk-versions-table>tbody>tr");
+		for (Element pRow : pTableArr) {
+			String tv = pRow.select("td>strong").get(0).text();
+			if (tv.contains("9999"))
+				continue;
+			else {
+				result = tv;
+				break;
+			}
+		}
+
+		return result;
+	}
+
+	public String createResultString(String name, String ver, String desc) {
+		return "[Gentoo] \u0002" + name + "\u0002 (" + ver + ") - " + desc;
+	}
+
+	public String createNoResultString() {
+		return "[Gentoo] 결과가 없습니다";
 	}
 }
